b26cd909c87b8ec5979bd79993ea98c13f4d1840
Bunmi <bunmioye09@gmail.com>
2020-11-12 08:50:45 +0100

    Updates

---
+++ precache-manifest.7e36e901e5be2a3f5a0ee97db033f334.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3155d45cad00a5d069adf9893cf46b1b",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "c3293a6d3c816f112451",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "0701261f1e45ef9193b0",
+    "url": "/robofriends/static/css/main.9fcf0284.chunk.css"
+  },
+  {
+    "revision": "c3293a6d3c816f112451",
+    "url": "/robofriends/static/js/2.33fbdc0c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/robofriends/static/js/2.33fbdc0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0701261f1e45ef9193b0",
+    "url": "/robofriends/static/js/main.3bdce038.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
