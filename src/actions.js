5baf2b9aeb4d8672f394b94d4a79426af2e87499
Bunmi <bunmioye09@gmail.com>
2020-11-12 08:38:56 +0100

    commented out some unnecessary code

--- src/actions.js
+++ src/actions.js
@@ -1,13 +1,13 @@
 import {
-    CHANGE_SEARCH_FIELD, 
-    FETCH_ROBOTS_REQUEST, 
-    FETCH_ROBOTS_SUCCESS, 
-    FETCH_ROBOTS_FAILURE, 
+  CHANGE_SEARCH_FIELD,
+  FETCH_ROBOTS_REQUEST,
+  FETCH_ROBOTS_SUCCESS,
+  FETCH_ROBOTS_FAILURE,
 } from './constants.js';
 
-export const setSearchField = (text) => ({
-    type: CHANGE_SEARCH_FIELD,
-    payload: text
+export const setSearchField = text => ({
+  type: CHANGE_SEARCH_FIELD,
+  payload: text,
 });
 
 // export const fetchRobotsRequest = () => {
@@ -44,10 +44,9 @@ export const setSearchField = (text) => ({
 // }
 
 export const fetchRobots = () => dispatch => {
-    dispatch({ type: FETCH_ROBOTS_REQUEST });
-    fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
-        .then(data => dispatch({ type: FETCH_ROBOTS_SUCCESS, payload: data }))
-        .catch(error => dispatch({ type: FETCH_ROBOTS_FAILURE, payload: error }))
-}
-
+  dispatch({ type: FETCH_ROBOTS_REQUEST });
+  fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(data => dispatch({ type: FETCH_ROBOTS_SUCCESS, payload: data }))
+    .catch(error => dispatch({ type: FETCH_ROBOTS_FAILURE, payload: error }));
+};
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,25 +2,21 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
+  // throw error for catch
+  // if (false) {
+  //     throw new Error('NOOOOO!');
+  // }
 
-    // throw error for catch
-    if (false) {
-        throw new Error('NOOOOO!');
-    }
-
+  return robots.map((user, i) => {
     return (
-        robots.map((user, i) => {
-            return (
-                <Card 
-                key={i} 
-                id={robots[i].id} 
-                name={robots[i].name} 
-                email={robots[i].email} 
-                />
-            );
-        })
+      <Card
+        key={i}
+        id={robots[i].id}
+        name={robots[i].name}
+        email={robots[i].email}
+      />
     );
-}
+  });
+};
 
 export default CardList;
-
